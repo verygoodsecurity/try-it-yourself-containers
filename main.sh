f0c0857538610f1516a16396a0a5a8173a389d45
Vitalii Liashchenko <vitalii.liashchenko@vgs.io>
2020-07-17 16:57:11 +0300

    Launch script fixes

--- main.sh
+++ main.sh
@@ -2,4 +2,4 @@
 BASH_JSON=$(docker-compose run try-it-bash)
 NODE_JSON=$(docker-compose run try-it-node)
 PYTHON_JSON=$(docker-compose run try-it-python)
-(echo $BASH_JSON; echo $NODE_JSON; echo $PYTHON_JSON) | jq -s add > try_it_results_false.json
+(echo $BASH_JSON; echo $NODE_JSON; echo $PYTHON_JSON) | jq -s add > try_it_results.json
